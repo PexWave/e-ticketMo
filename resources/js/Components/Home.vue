5bf8a6b9df3a3b41c3a3b0ee121c8d43e059d202
Inayshi <107098935+Inayshi@users.noreply.github.com>
2024-03-11 12:52:09 +0800

    sign in page 60%

--- resources/js/Components/Home.vue
+++ resources/js/Components/Home.vue
@@ -1,22 +1,22 @@
 <template>
-    <v-container>
-        <v-row justify="center">
-            <v-col cols="12" sm="8" md="6">
-                <v-card>
-                    <v-card-text>
-                        <h1 class="text-center">Home Page</h1>
-                        <br /><br />
-                        <v-row justify="center" class="text-center">
-                            <v-btn color="primary" class="mx-3" href="/signin"
-                                >Sign In</v-btn
-                            >
-                            <v-btn color="primary" class="mx-3" href="/signup"
-                                >Sign Up</v-btn
-                            >
-                        </v-row>
-                    </v-card-text>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+   <v-container class="mx-0 my-0 px-0 py-0">
+    <v-row>
+      <!-- Logo Column -->
+      <v-col cols="6" class="logo-col d-flex justify-center align-center w-screen">
+        <img src="images/LOGO-BLUE.png" class="w-80">
+      </v-col>
+
+      <!-- Sign In Column -->
+      <v-col cols="6" class="sign-in-col w-full">
+      
+        <v-form class="w-100">
+        <h1 class="header text-center"> Sign In </h1>
+          <v-text-field v-model="username" label="Username"></v-text-field>
+          <v-text-field v-model="password" label="Password" type="password"></v-text-field>
+          <v-btn class="w-100"color="primary" @click="signIn">Sign In</v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
