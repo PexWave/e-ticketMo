7768a547dec96d1a4559bd7620fe246e2cd08a30
pexwave <asakilsarhan@gmail.com>
2024-03-12 09:37:54 +0800

    added crud for role

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+//ROLE ROUTE
+Route::apiResource('/role', RoleController::class);
+Route::controller(RoleController::class)->group(function() {
+    Route::post('/role','store');
+    Route::put('/role/{id}','update');
+    //specific resource
+    Route::get('/role/{id}','show');
 });
