e541867a26a7c0aec5c1b596bc02a70acd89fd51
pexwave <asakilsarhan@gmail.com>
2024-04-24 15:04:56 +0800

    added modal

--- resources/js/Components/Client/Components/TicketQueues.vue
+++ resources/js/Components/Client/Components/TicketQueues.vue
@@ -1,87 +1,36 @@
 <template>
-    <v-table density="compact">
-      <thead>
-        <tr>
-          <th class="text-left">
-            Queue #
-          </th>
-          <th class="text-left">
-            Ticket #
-          </th>
-          <th class="text-left">
-            Client Type
-          </th>
-          <th class="text-left">
-            Extension Request
-          </th>
-          <th class="text-left">
-            Transfer Request
-          </th>
-          <th class="text-left">
-            Status
-          </th>
-          <th class="text-left">
-            Action
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in desserts"
-          :key="item.name"
-        >
-          <td>{{ item.name }}</td>
-          <td>{{ item.calories }}</td>
-        </tr>
-      </tbody>
-    </v-table>
+<v-dialog content-class="transfer-ticket-container">
+  <template v-slot:activator="{ props: activatorProps }">
+    <v-btn
+      v-bind="activatorProps"
+      color="surface-variant"
+      text="Open Dialog"
+      variant="flat"
+    ></v-btn>
   </template>
+
+  <template v-slot:default="{ isActive }">
+    <v-card>
+
+    <i class="fa fa-2xl cursor-pointer fa-times close-btn" text="Close Dialog"
+        @click="isActive.value = false" aria-hidden="true"></i>
+
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
+</template>
   <script>
+    import "../../../../css/client.css";
+
     export default {
       data () {
         return {
-          desserts: [
-            {
-              name: 'Frozen Yogurt',
-              calories: 159,
-            },
-            {
-              name: 'Ice cream sandwich',
-              calories: 237,
-            },
-            {
-              name: 'Eclair',
-              calories: 262,
-            },
-            {
-              name: 'Cupcake',
-              calories: 305,
-            },
-            {
-              name: 'Gingerbread',
-              calories: 356,
-            },
-            {
-              name: 'Jelly bean',
-              calories: 375,
-            },
-            {
-              name: 'Lollipop',
-              calories: 392,
-            },
-            {
-              name: 'Honeycomb',
-              calories: 408,
-            },
-            {
-              name: 'Donut',
-              calories: 452,
-            },
-            {
-              name: 'KitKat',
-              calories: 518,
-            },
-          ],
+ 
         }
       },
     }
