2867d23942e5dbbabf16c74dc0229ad43a26273b
Jasmin In-naka <jasmininnaka@gmail.com>
2024-04-24 15:14:04 +0800

    updated some code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,9 +10,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <link
+        {{-- <link
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-            rel="stylesheet"
+            rel="stylesheet" --}}
             />
 
         @vite(['resources/css/app.css','resources/js/app.js'])
